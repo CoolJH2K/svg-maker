879c7009f44aa07acb836c12e7964d4d6e1a2f2c
Janna Harmon <harmonjanna00@gmail.com>
2023-04-23 21:29:22 -0400

    Added all the prompts needed to generate an SVG image.

--- index.js
+++ index.js
@@ -3,7 +3,31 @@ const inquirer = require("inquirer");
 class CLI {
     run () {
         return inquirer
-            .prompt ([])
+            .prompt ([
+                {
+                    name: "text",
+                    message: "Which letters do you want on your logo?",
+                    type: "input",
+                },
+                {
+                    name: "textColor",
+                    message: "What color would you like the text to be?",
+                    type: "list",
+                    choices: ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White"],
+                },
+                {
+                    name: "shapeType",
+                    message: "What shape would you like your logo to be?",
+                    type: "list",
+                    choices: ["Circle", "Square", "Triangle"],
+                },
+                {
+                    name: "shapeColor",
+                    message: "What color would you like the shape to be?",
+                    type: "list",
+                    choices: ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White"],
+                }
+            ])
             .then (({text, textColor, shapeType, shapeColor}) => {})
             .then (() => {})
             .catch((error => {
