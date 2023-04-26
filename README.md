770e0408517d09b624a8c662fe1fc1516c8323a0
Janna Harmon <harmonjanna00@gmail.com>
2023-04-25 22:37:10 -0400

    Added lots of stuff in each of the JS files and updated the README.

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
-# svg-maker
+# SVG Maker
 ## Description
+Want to make your very own SVG image? That's where this handy tool comes in! Just use your terminal to answer a couple of questions, and a custom SVG logo will be made just for you!
 ### Why did I make this?
+I wanted to make a simple and easy way for people to create a simple SVG image. Making SVG images using HTML is fun, but it can take quite a while to get the exact kind of SVG image you want.
 ### What problem does this solve?
+This simplifies the creation of a simple SVG image. And who knows? Maybe after a while, those who find this helpful might want to try to step outside of their comfort zone and learn how they can create even more shapes!
 ### What did I learn?
+I learned a lot about how Inquirer prompts work and how they work with the FS/promises module to make the prompts function properly. Learning about how SVG images are made was also very fun!
 ## Video Demonstration
 ## Installation
+1. Install [node.js](https://nodejs.org/en/download). This comes with npm preinstalled.
+2. Open the terminal and enter "npm i inquirer@8.2.4" to istall Inquirer.
+3. Once Inquirer is finished installing, enter "npm i fs" to install the File Stystem methods.
+4. After Inquirer and FS are installed, type "node index.js" to start the application!
 ## Credits
+The people who helped me out with this:
+* https://github.com/JesseEmerson7
 ## License
 This application uses the MIT License
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+// Import Inquirer, fs and the needed objects from other JavaScript files
 const inquirer = require("inquirer");
+const fs = require("fs/promises");
 const SVG = require("./lib/svg");
+const {Circle, Triangle, Square} = require("./lib/shapes");
 
+// Set up the promts needed to generate an SVG image
 class CLI {
     run () {
         return inquirer
+        // Set up the questions
             .prompt ([
                 {
                     name: "text",
@@ -29,13 +34,40 @@ class CLI {
                     choices: ["Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Black", "White"],
                 }
             ])
-            .then (({text, textColor, shapeType, shapeColor}) => {})
-            .then (() => {})
+            //.then (({text, textColor, shapeType, shapeColor}) => {})
+            .then ((prompts) => {
+                if (prompts.text.length > 3) {
+                    console.log("Text must not exceed 3 letters.");
+                    promptUser();
+                } else {
+                    /*let svg = new SVG(prompt.text, prompt.textColor);
+                    svg.setText(prompts.text, prompts.textColor);*/
+
+                    var newShape;
+                    if (prompts.shape === "Circle") {
+                        newShape = new Circle(prompts.shapeColor);
+                        svg.setShape(Circle);
+                        svg.shapeElement += `<circle cx="150" cy="100" r="80" fill="${shapeColor}">`;
+                    } else if (prompts.shape === "Triangle") {
+                        newShape = new Triangle(prompts.shapeColor);
+                        svg.setShape(Triangle);
+                        svg.shapeElement += `<polygon points="150, 18, 244, 182, 56, 182" fill"${shapeColor}">`;
+                    } else if (prompts.shape === "Square") {
+                        newShape = new Square(prompts.shapeColor);
+                        svg.setShape(Square);
+                        svg.shapeElement += `<rect x="90" y="40" width="120" height="120" fill="${shapeColor}">`;
+                    }
+                    console.log(newShape);
+                    let svg = new SVG(newShape.render(), prompts.text, prompts.textColor);
+                    fs.writeFile(`logo.svg`, svg.render());
+                }
+            })
             .catch((error => {
                 console.log(error);
             }))
     }
 }
 
+// Runs the prompts
 const cli = new CLI;
 cli.run();
